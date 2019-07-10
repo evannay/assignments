ab5b5fa5014b77b778e4d7bd8ddc6fda2c37877c
Evan Nay <enay1248@gmail.com>
2019-07-10 14:24:24 -0600

    css-wars

---
+++ exercises/hello-class/app.js
@@ -0,0 +1,33 @@
+// var sayHello = function(){
+//     console.log("hello class")
+// }
+
+// sayHello()
+
+// function sayHello() {
+//     console.log("hello class")
+// }
+
+// sayHello()
+
+//level two
+
+// function sayHello(name) {
+//     console.log("Hello " + name +"!")
+// }
+
+// sayHello('John')
+
+//level three
+
+// function sayHello(name, age) {
+//     console.log("Hello " + name + ", how do you like being " + age + "!")
+// }
+
+// sayHello('John', '30')
+
+function sayHello(name){
+    console.log("Hello " + name + " !")
+}
+
+sayHello('Evan')
