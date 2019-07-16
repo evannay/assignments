d34e5c91df99d015854296172cca2d27bebe7b29
Evan Nay <enay1248@gmail.com>
2019-07-16 14:04:10 -0600

    fixed it

--- exercises/arrayMethods/app.js
+++ exercises/arrayMethods/app.js
@@ -1,18 +1,35 @@
-var fruit = ["banana", "apple", "orange", "watermelon"];
-var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
-vegetables.pop()
-fruit.shift()
-let indexOrange = fruit.indexOf('orange')
-fruit.push(1)
-let vLength = vegetables.length
-vegetables.push(3)
-// let strFruit = fruit.join(' ')
-// let strVeg = vegetables.join(' ')
-// let total = strFruit.concat(strVeg)
-let food = fruit.concat(vegetables)
-food.splice(4,2)
-food.reverse()
-let newFood= food.join(',')
-console.log(newFood)
-console.log(fruit)
-console.log(vegetables)
+// var fruit = ["banana", "apple", "orange", "watermelon"];
+// var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+// vegetables.pop()
+// fruit.shift()
+// let indexOrange = fruit.indexOf('orange')
+// fruit.push(1)
+// let vLength = vegetables.length
+// vegetables.push(3)
+// // let strFruit = fruit.join(' ')
+// // let strVeg = vegetables.join(' ')
+// // let total = strFruit.concat(strVeg)
+// let food = fruit.concat(vegetables)
+// food.splice(4,2)
+// food.reverse()
+// let newFood= food.join(',')
+// console.log(newFood)
+// console.log(fruit)
+// console.log(vegetables)
+
+// var fruit = ["banana", "apple", "orange", "watermelon"];
+// var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+// const food = fruit.concat(vegetables)
+// const myFood = food.slice(2,6)
+// myFood.push("Beer", "Toilet Paper")
+// for(let i = 0; i < myFood.length; i++){
+//     if(myFood[i] === "Beer") {
+//         myFood.push("Wings", "Ranch Dressing")
+//     } else if(myFood[i] === "Wings") {
+//         myFood.push("More Wings")
+//     }
+// }
+
+// console.log(myFood)
+
+
