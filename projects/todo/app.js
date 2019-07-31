5e1ef34b70637a935227a32214e8c4c85a2ab851
Evan Nay <enay1248@gmail.com>
2019-07-31 08:13:27 -0600

    project

--- projects/todo/app.js
+++ projects/todo/app.js
@@ -1,4 +1,3 @@
-
 // axios.get('https://api.vschool.io/evan/todo/').then((response)=>{
 //     response.data.forEach(toDo =>{
 //         let li = document.createElement('li')
@@ -7,6 +6,115 @@
 
 //     })
 // })
+
+
+
+
+// function displayToDo(){
+//     axios.get('https://api.vschool.io/evan/todo/').then(response =>{
+//     response.data.forEach(toDo =>{
+//         let li = document.createElement('li')
+//         li.textContent = toDo.title
+//         let markOff = document.createElement("input")
+//         markOff.setAttribute('type', 'checkbox')
+//         let eraseButton = document.createElement('button')
+//         eraseButton.textContent = 'X'
+//         eraseButton.addEventListener('click', e =>{
+//             console.log('fired')
+//             eraseToDo(toDo, li)
+//         })
+//         if(toDo.completed === true){
+//             li.style.textDecoration = 'line-through'
+//         }
+//         let picture = document.createElement('img')
+//         picture.setAttribute('class', 'pic')
+//         picture.src = toDo.imgUrl
+//         li.appendChild(markOff)
+//         document.getElementById('list').appendChild(li)
+//         li.appendChild(picture)
+//         li.appendChild(eraseButton)
+//     })
+// })
+// }
+// displayToDo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.addTodo.addEventListener('submit', function(e){
     e.preventDefault()
@@ -19,34 +127,49 @@ document.addTodo.addEventListener('submit', function(e){
     posting(newTodo)
 })
 
-axios.get('https://api.vschool.io/evan/todo/').then(response =>{
-    displayTodos(response.data)
-})
+function start (updated){
+    axios.get('https://api.vschool.io/evan/todo/').then(response =>{
+        displayTodos(response.data, updated)
+    })
+}
+start()
 
-function displayTodos (todos){
+function displayTodos (todos, updated){
+    if(updated){
+        document.getElementById('list').innerHTML = ''
+    }
     todos.forEach(toDo =>{
         let li = document.createElement('li');
         li.textContent = toDo.title;
         let markOff = document.createElement("input")
         markOff.setAttribute('type', 'checkbox')
+        let eraseButton = document.createElement('button')
+        eraseButton.textContent = 'X'
+        eraseButton.addEventListener('click', e =>{
+            eraseToDo(toDo, li)
+        })
         let p = document.createElement("p")
         p.textContent = toDo.description
         let showPrice = document.createElement("p")
         showPrice.textContent = `Price: ${document.addTodo.price.value}`
+        let picture = document.createElement('img')
+        picture.setAttribute('class', 'pic')
+        picture.src = toDo.imgUrl
         markOff.addEventListener('click', function (e) {  
-            axios.put(`https://api.vschoo.io/evan/todo/${editedTodo_id}`).then(response =>{
-                toDo.complete = true
-            })
-            //put request to change the value of completed to true
+            updating(toDo)
         })
         if(toDo.completed === true){
+            markOff.checked = true
             li.style.textDecoration = 'line-through'
         }
         li.appendChild(markOff)
         document.getElementById('list').appendChild(li);
         li.appendChild(p)
         li.appendChild(showPrice)
+        li.appendChild(picture)
+        li.appendChild(eraseButton)
     })
+    
 }
 
 function posting(newPost){
@@ -55,10 +178,14 @@ function posting(newPost){
     })
 }
 
-// axios.get('https://api.vschool.io/evan/todo/').then(response =>{
-//     response.data.forEach(todo => {
-//         let li = document.createElement('li')
-//         li.textContent = todo.title
-//         document.getElementById('title').appendChild(li)
-//     })
-// })
+function updating(toDo){
+    axios.put(`https://api.vschool.io/evan/todo/${toDo._id}`, {completed: !toDo.completed}).then(response =>{
+        start(true)
+        console.log(toDo)
+    })
+}
+
+function eraseToDo(toDo, li){
+    axios.delete(`https://api.vschool.io/evan/todo/${toDo._id}`)
+    document.getElementById('list').removeChild(li)
+}
