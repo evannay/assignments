c54e06de260b1d23c6a8b54cfa5e9c5ea42d3d16
Evan Nay <enay1248@gmail.com>
2019-09-09 12:30:31 -0600

    zen

--- exercises/hunterofbounties/client/src/Form.js
+++ exercises/hunterofbounties/client/src/Form.js
@@ -4,17 +4,33 @@ class Form extends React.Component {
     constructor(){
         super()
         this.state = {
-
+            firstName: '',
+            lastName: '',
+            bountyPayout: '',
+            type: ''
         }
     }
 
-
+    handleChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault()
+    }
 
     render(){
         return(
-            <div>
-                Hello
-            </div>
+            <form className='form-container' onSubmit={this.handleSubmit}>
+                <input type="text" placeholder='First Name' name='firstName' onChange={this.handleChange}/>
+                <input type="text" placeholder='Last Name' name='lastName' onChange={this.handleChange}/>
+                <input type="number" placeholder='Bounty Payout' name='bountyPayout' onChange={this.handleChange}/>
+                <input type='radio' name='type' />Sith
+                <input type='radio' name='type' />Jedi
+            </form>
         )
     }
 }
