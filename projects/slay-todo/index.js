9f4b9c333ee1ce735cea724bd39e4839e51bf88a
Evan Nay <enay1248@gmail.com>
2019-10-22 10:39:15 -0600

    login signup routes

--- projects/slay-todo/index.js
+++ projects/slay-todo/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const PORT = 8088
 const mongoose = require('mongoose')
 
 app.use('/', express.json())
+app.use('/auth', require('./routes/authRoutes'))
 app.use('/todos', require('./routes/todoRoutes'))
 
 mongoose.connect('mongodb://localhost:27017/epic-todos', {
---
+++ projects/slay-todo/routes/authRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const User = require('../models/UserSchema')
+const authRouter = express.Router()
+const jwt = require('jsonwebtoken')
+
+authRouter.route('/signup')
+
+    .post((req, res, next) => {
+        User.findOne({username: req.body.username}, (err, existingUser) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            if(existingUser !== null){
+                res.status(400)
+                return next(new Error("That username already exists!"))
+            }
+            const newUser = new User(req.body)
+            newUser.save((err, user) => {
+                if(err){
+                    res.status(500)
+                    return next(err)
+                }
+                const token = jwt.sign(user.toObject(), process.env.SECRET)
+                return res.status(201).send({success: true, user: user.toObject(), token})
+            })
+        })
+    })
+
+authRouter.route('/login')
+
+    .post((req, res, next) => {
+        User.findOne({username: req.body.username.toLowerCase()}, (err, user) => {
+            if(err){
+                return next(err)
+            }
+            if(!user || user.password !== req.body.password){
+                res.status(403)
+                return next(new Error("username or password are incorrect"))
+            }
+            const token = jwt.sign(user.toObject(), process.env.SECRET)
+            return res.send({token: token, user: user.toObject(), success: true})
+        })
+    })
+
+module.exports = authRouter
